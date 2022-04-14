8a09bb21965c7b3ee3f3e63969c0be10bb95c5b1
pizzacodr <38480755+pizzacodr@users.noreply.github.com>
2022-04-14 15:27:30 -0400

    Fixed exceptions

--- src/main/java/com/github/pizzacodr/rsstodb/App.java
+++ src/main/java/com/github/pizzacodr/rsstodb/App.java
@@ -7,7 +7,7 @@ import org.aeonbits.owner.ConfigFactory;
 import com.rometools.rome.io.FeedException;
 
 public class App {
-    public static void main( String[] args ) throws IllegalArgumentException, FeedException, IOException, InterruptedException, SQLException {
+    public static void main( String[] args ) throws FeedException, IOException, InterruptedException, SQLException {
     	
     	ConfigFile configFile = ConfigFactory.create(ConfigFile.class, System.getProperties());
     	new RssProcessor().processFeed(configFile);
